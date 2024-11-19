f475c3841707e3d727bc61102e1579479031963e
yashaswini123915 <mudragaddarajaniyashaswini@gmail.com>
2024-11-19 19:38:40 +0530

    Add files via upload
    uploading to test the extention to main branch

---
+++ python.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 11:44:14 2024
+
+@author: ADMIN
+"""
+class Car():
+    def__init__(self,x):
+        str x
+        print(x)
+make = Car("HONADA")
+model = Car("ACCORD")
+color = Car("blue")      
